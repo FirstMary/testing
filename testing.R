1f32ed0472878499cfa2999218ff8e07dee9c3ba
FirstMary <govoruha.mariya@gmail.com>
2020-05-12 13:37:39 +0300

    додала вар 2
    варрррр варрррр

--- testing.R
+++ testing.R
@@ -2,3 +2,4 @@ git remote add origin https://github.com/FirstMary/testing.git
 git push -u origin master
 print("This file was created within RStudio. And now it lives on GitHub")
 my_var <- 3
+my_var_2 <- 23
