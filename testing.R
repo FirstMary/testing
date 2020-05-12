8c8795c1e042022bf01e346b85ced2631a42d1b6
FirstMary <govoruha.mariya@gmail.com>
2020-05-12 13:44:34 +0300

    змінила значення вар2, додала вар7
    вар2 видалився і сторився новий

--- testing.R
+++ testing.R
@@ -2,4 +2,5 @@ git remote add origin https://github.com/FirstMary/testing.git
 git push -u origin master
 print("This file was created within RStudio. And now it lives on GitHub")
 
-my_var_2 <- 23
+my_var_2 <- 27
+my_var_7 <- my_var_2-2
