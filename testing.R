856d9c64804610740018d61621d9a0df93e2d3bf
FirstMary <govoruha.mariya@gmail.com>
2020-05-12 13:40:43 +0300

    видалила вар *1*
    тепер червоний

--- testing.R
+++ testing.R
@@ -1,5 +1,5 @@
 git remote add origin https://github.com/FirstMary/testing.git
 git push -u origin master
 print("This file was created within RStudio. And now it lives on GitHub")
-my_var <- 3
+
 my_var_2 <- 23
