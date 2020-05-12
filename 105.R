669177e058a61a553377126012016c7f015675c2
FirstMary <govoruha.mariya@gmail.com>
2020-05-12 13:34:10 +0300

    created 2 vars
    создала вар1 и вар2

---
+++ 105.R
@@ -0,0 +1,4 @@
+
+print("This file was created within RStudio. And now it lives on GitHub")
+my_var1 <- 3
+my_var_2 <- 23
---
+++ testing.R
@@ -0,0 +1,4 @@
+git remote add origin https://github.com/FirstMary/testing.git
+git push -u origin master
+print("This file was created within RStudio. And now it lives on GitHub")
+my_var <- 3
